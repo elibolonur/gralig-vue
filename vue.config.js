d649830eae7775737158fced0a8f25c3509d4138
Onur Elibol <korax11@hotmail.com>
2021-07-05 22:44:25 +0200

    fix: move scss imports to global scope

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "gralig/scss/abstracts/all.scss";
+          @import "gralig/scss/modes/modes.scss";
+          @import "gralig/scss/base/all.scss";
+          @import "gralig/scss/grid/all.scss";
+        `,
+      },
+    },
+  },
 }
