82a19ce7c1ae566501b37a636d44071d38a8a3c4
Onur Elibol <korax11@hotmail.com>
2021-09-06 22:27:03 +0200

    chore: split GListItem to its own folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as GButton } from './GButton/GButton.vue'
 export { default as GProgress } from './GProgress/GProgress.vue'
 export { default as GList } from './GList/GList.vue'
-export { default as GListItem } from './GList/GListItem.vue'
+export { default as GListItem } from './GListItem/GListItem.vue'
 export { default as GDropdown } from './GDropdown/GDropdown.vue'
 export { default as GDropdownItem } from './GDropdown/GDropdownItem.vue'
 export { default as GLoader } from './GLoader/GLoader.vue'
