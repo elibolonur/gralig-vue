39a93cdaabbfb4a29ccb30a02f511d5e97fc6313
Onur Elibol <korax11@hotmail.com>
2021-09-06 23:36:57 +0200

    feat: add Checkbox component

---
+++ playground/examples/GCheckbox.ex.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="g-checkbox-example">
+    <h4 class="header my-3">Checkboxes => (GCheckbox.vue)</h4>
+
+    <!-- Default -->
+    <h5 class="header mb-2">Example</h5>
+    <GCheckbox v-model="model">vue:checkbox</GCheckbox>
+    <GCheckbox disabled>vue:checkbox - disabled</GCheckbox>
+
+    <div class="mb-6" />
+
+    <!-- Colors -->
+    <h5 class="header mb-2">Colors</h5>
+    <GCheckbox color="blue">Blue</GCheckbox>
+    <GCheckbox color="yellow">Yellow</GCheckbox>
+    <GCheckbox color="green">Green</GCheckbox>
+    <GCheckbox color="red">Red</GCheckbox>
+
+    <div class="mb-6" />
+  </div>
+</template>
+
+<script>
+import { GCheckbox } from '@/components'
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'CheckboxEx',
+  components: {
+    GCheckbox,
+  },
+  setup() {
+    const model = ref(false)
+
+    return { model }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.g-checkbox-example {
+}
+</style>
---
+++ src/components/GCheckbox/GCheckbox.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-bind="checkboxAttrs" @click="$emit('click')">
+    <input
+      :id="randomID"
+      class="gra-custom-form-input"
+      type="checkbox"
+      :disabled="checkboxAttrs.disabled"
+      :checked="value"
+      @click="$emit('input', !value)"
+    />
+    <label class="gra-custom-form-label" :for="randomID">
+      <slot />
+    </label>
+  </div>
+</template>
+
+<script>
+import { computed, defineComponent } from '@vue/composition-api'
+import { uid, validateColor } from '../../helpers'
+
+export default defineComponent({
+  name: 'GCheckbox',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+    },
+    color: {
+      type: String,
+      validator: validateColor,
+    },
+  },
+  emits: ['input', 'click'],
+  setup(props) {
+    const checkboxAttrs = computed(() => ({
+      class: {
+        'gra-custom-form-control': true,
+        'gra-checkbox': true,
+        [`${props.color}`]: !!props.color,
+      },
+      value: props.value,
+      disabled: props.disabled,
+    }))
+
+    const randomID = computed(() => uid(1, 'id-'))
+
+    return { checkboxAttrs, randomID }
+  },
+})
+</script>
+
+<style lang="scss">
+@import 'gralig/scss/components/_checkbox.scss';
+@import 'gralig/scss/components/_form.scss';
+</style>
