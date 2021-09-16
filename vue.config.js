c95132e70df098fc0d2aa508cf905290278e127f
Onur Elibol <korax11@hotmail.com>
2021-09-17 01:42:56 +0200

    feat: import all styles once through js inject plugin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const InjectPlugin = require('webpack-inject-plugin').default
+
 module.exports = {
   lintOnSave: false,
   publicPath: '/gralig-vue',
@@ -7,10 +9,17 @@ module.exports = {
         additionalData: `
           @import "gralig/scss/abstracts/all.scss";
           @import "gralig/scss/modes/modes.scss";
-          @import "gralig/scss/base/all.scss";
           @import "gralig/scss/grid/all.scss";
         `,
       },
     },
+  },
+  configureWebpack: {
+    plugins: [
+      new InjectPlugin(() => {
+        // Import all.scss file only once through JS entry
+        return "import 'gralig/scss/base/all.scss'"
+      }),
+    ],
   },
 }
